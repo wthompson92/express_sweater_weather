ba4d9cfae1f9fa3db33c179d2c61a8c344476046
username <wnt925@gmail.com>
2019-10-07 03:41:54 -0600

    Add api key validation

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -11,36 +11,36 @@ var googleApiKey = process.env.GOOGLE_API_KEY
 
 router.get('/', function(req, res, next) {
   var payload;
- users.findOne({where: {api_key: req.body.api_key}})
- .then(user => {
-   if (user) {
-  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.address}&key=${googleApiKey}`)
-    .then(res => {return res.json()})
-    .then(result => {
-      coordinates = {
-        lat: result['results'][0]['geometry']['location']['lat'],
-        lng: result['results'][0]['geometry']['location']['lng'] }
-        return (coordinates)})
+  users.findOne({where: {api_key: req.body.api_key}})
+  .then(user => {
+    if (user) {
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.address}&key=${googleApiKey}`)
+        .then(res => {return res.json()})
+        .then(result => {
+          coordinates = {
+            lat: result['results'][0]['geometry']['location']['lat'],
+            lng: result['results'][0]['geometry']['location']['lng'] }
+            return (coordinates)})
 
-    .then (coordinates => fetch(`https://api.darksky.net/forecast/${darkskyApiKey}/${coordinates['lat']},${coordinates['lng']}`))
-      .then(response => response.json())
-      .then(response => {
-          forecast = {
-            currently: response.currently,
-            hourly: response.hourly,
-            daily: response.daily
-          }
-       res.status(200).send(forecast)
-    })
-}
-else {
-      payload = {
-        error: "API key is not valid",
-        status: 401
-      }
-      res.status(401).send(payload)
-    }
-  })
-});
+            .then (coordinates => fetch(`https://api.darksky.net/forecast/${darkskyApiKey}/${coordinates['lat']},${coordinates['lng']}`))
+              .then(response => response.json())
+              .then(response => {
+                forecast = {
+                  currently: response.currently,
+                  hourly: response.hourly,
+                  daily: response.daily
+                }
+                res.status(200).send(forecast)
+              })
+            }
+            else {
+              payload = {
+                error: "API key is not valid",
+                status: 401
+              }
+              res.status(401).send(payload)
+            }
+          })
+        });
 
-  module.exports = router;
+        module.exports = router;
