039c4c9097f71ac66008329e31e159feba2193a2
username <wnt925@gmail.com>
2019-10-07 03:41:15 -0600

    Add api key validation

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -3,12 +3,17 @@ var router = express.Router();
 var fetch = require("node-fetch");
 var express = require('express');
 var router = express.Router();
+var users = require('../../../models').User
 require('dotenv').config();
 
 var darkskyApiKey = process.env.DARKSKY_API_KEY
 var googleApiKey = process.env.GOOGLE_API_KEY
 
 router.get('/', function(req, res, next) {
+  var payload;
+ users.findOne({where: {api_key: req.body.api_key}})
+ .then(user => {
+   if (user) {
   fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.address}&key=${googleApiKey}`)
     .then(res => {return res.json()})
     .then(result => {
@@ -27,6 +32,15 @@ router.get('/', function(req, res, next) {
           }
        res.status(200).send(forecast)
     })
+}
+else {
+      payload = {
+        error: "API key is not valid",
+        status: 401
+      }
+      res.status(401).send(payload)
+    }
+  })
 });
 
   module.exports = router;
