389cc77c68e54d1a78e8f5957ce3221b895cc282
username <wnt925@gmail.com>
2019-10-07 03:36:22 -0600

    Add working forecasts

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -8,21 +8,25 @@ require('dotenv').config();
 var darkskyApiKey = process.env.DARKSKY_API_KEY
 var googleApiKey = process.env.GOOGLE_API_KEY
 
-router.get('/', function(request, response, next) {
-  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.address}&key=${googleApiKey}`)
-    .then(response => {return response.json()})
+router.get('/', function(req, res, next) {
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.address}&key=${googleApiKey}`)
+    .then(res => {return res.json()})
     .then(result => {
       coordinates = {
-        location: request.query.address,
         lat: result['results'][0]['geometry']['location']['lat'],
         lng: result['results'][0]['geometry']['location']['lng'] }
         return (coordinates)})
 
-        .then(coordinates => fetch(`https://api.darksky.net/forecast/${darkskyApiKey}/${coordinates['lat']},${coordinates['lng']}`))
+    .then (coordinates => fetch(`https://api.darksky.net/forecast/${darkskyApiKey}/${coordinates['lat']},${coordinates['lng']}`))
+      .then(response => response.json())
+      .then(response => {
+          forecast = {
+            currently: response.currently,
+            hourly: response.hourly,
+            daily: response.daily
+          }
+       res.status(200).send(forecast)
+    })
+});
 
-          .then(response => response.json())
-          console.log(response)
-        });
-
-
-        module.exports = router;
+  module.exports = router;
